94f8496072872d8ee8d690f61de91f937c30a574
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-01 01:11:55 -0500

    Update base for DB with SqlAlchemy

--- src/models/base.py
+++ src/models/base.py
@@ -1,9 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+from contextlib import contextmanager
+from functools import wraps
+from threading import Lock
 
-# TODO Change for some other DB at some point
-ENGINE = create_engine('sqlite://script.sqlite', echo=True)
 
 # BASE for schemas
 BASE = declarative_base()
+
+# TODO Change for some other DB at some point
+_engine = create_engine('sqlite://script.sqlite', echo=True)
+
+sqlite_mutex = Lock()
+
+
+@contextmanager
+def get_session(auto_commit: bool = True) -> Session:
+    with sqlite_mutex:
+        session = Session(_engine)
+        try:
+            yield session
+        except:
+            if auto_commit:
+                session.commit()
