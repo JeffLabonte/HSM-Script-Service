c6e24fbc4cabb398bf2fd3f0984af3c2cae7b2c4
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-01 01:12:27 -0500

    First version of schema

--- src/models/scripts.py
+++ src/models/scripts.py
@@ -7,3 +7,8 @@ class Scripts(BASE):
     __tablename__ = 'scripts'
 
     id = Column(Integer, primary_key=True)
+    name = Column("script_name", String(64))
+    path = Column("script_path", String(256))
+
+    def __repr__(self):
+        return f"<Script(id={self.id}, name={self.name}, path={self.path})>"
