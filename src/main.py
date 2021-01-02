9a148baa8801252ab887e5d3e441c2c0bc79114c
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-02 02:27:18 -0500

    First try to AMQP using topics

--- src/main.py
+++ src/main.py
@@ -1,2 +1,30 @@
+import pika
+
+
+TOPIC_TO_RECEIVE = 'script.#'
+
+
 if __name__ == "__main__":
-    pass
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='broker')
+    )
+
+    channel = connection.channel()
+    channel.exchange_declare(exchange='script_topic', exchange_type='topic')
+
+    result = channel.queue_declare('', exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange='script_topic',
+                       queue=queue_name, routing_key=TOPIC_TO_RECEIVE)
+
+    def callback(ch, method, properties, body):
+        print("test")
+
+    channel.basic_consume(
+        queue=queue_name,
+        on_message_callback=callback,
+        auto_ack=True
+    )
+
+    channel.start_consuming()
