7a7c54c40be6da953569196ce1ee7acdb380d36c
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-02 22:02:38 -0500

    Add another test for helper

--- src/helper/amqp.py
+++ src/helper/amqp.py
@@ -1,7 +1,14 @@
 import json
+import logging
 from typing import Dict, ByteString
+
+logger = logging.getLogger(f'script_manager.{__name__}')
 
 
 def get_payload_from_body(body: ByteString) -> Dict:
-    decoded_body = body.decode('utf-8')
-    return json.loads(decoded_body)
+    try:
+        decoded_body = body.decode('utf-8')
+        return json.loads(decoded_body)
+    except json.decoder.JSONDecodeError:
+        logger.exception("Issue on decoding")
+        raise
--- src/tests/unit/helper/test_amqp.py
+++ src/tests/unit/helper/test_amqp.py
@@ -1,3 +1,7 @@
+import pytest
+
+from json.decoder import JSONDecodeError
+
 from helper.amqp import get_payload_from_body
 
 
@@ -7,3 +11,10 @@ def test_get_payload_from_body__pass_json__should_return_dict():
 
     assert isinstance(json_dict, dict)
     assert json_dict.get("test") == 1
+
+
+def test_get_payload_from_body__pass_non_json__should_raise_exception():
+    body = b"This is test"
+
+    with pytest.raises(JSONDecodeError):
+        get_payload_from_body(body)
