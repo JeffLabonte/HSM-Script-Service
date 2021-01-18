0c9a2ef6b6c4fe5ca085d1cccca3582ce8525277
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-18 00:07:47 -0500

    Remove mapping, planning something else

--- src/main.py
+++ src/main.py
@@ -8,13 +8,6 @@ logger = logging.getLogger('script_service')
 
 TOPIC_TO_CONSUME = 'script.#'
 
-TOPIC_TO_FUNCTION_MAPPING = {
-    'script.get': None,  # TODO Add function from controller
-    'script.get.by_attr': None,
-    'script.create': None,
-    'script.update.id.$': None,
-}
-
 
 def callback(ch, method, properties, body):
     payload = get_payload_from_body(body)
