c539138d85dfd41bfc06f54c49de3a8ff7f55e55
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-18 23:17:30 -0500

    Document the validator

--- src/validator.py
+++ src/validator.py
@@ -5,6 +5,9 @@ from typing import Dict
 
 
 def validate_script_schema(dirty_input: dict) -> Dict:
+    """
+    Empty dict means that there no errors.
+    """
     v = Validator()
     v.validate(dirty_input, SCRIPT_SCHEMA)
     return v.errors
