499d07ed7e7a3d18d04b117af2e334dac40d355c
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-18 23:16:46 -0500

    Update schema
    Keep it flexible while being strict

--- src/schemas/scripts.py
+++ src/schemas/scripts.py
@@ -4,4 +4,13 @@ SCRIPT_SCHEMA = {
         "required": True,
         "allowed": ['POST', 'GET', 'PUT', 'DELETE'],
     },
+    "transaction_id": {
+        "required": True,
+        "regex": "^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$",
+    },
+    "data": {
+        "type": "dict",
+        "required": True,
+        "empty": False
+    },
 }
