757339a3b3b3a3f27ac296ba340d69a19e069b33
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-18 23:35:50 -0500

    Create test for invalid data

--- src/tests/unit/schemas/test_scripts.py
+++ src/tests/unit/schemas/test_scripts.py
@@ -9,7 +9,7 @@ import pytest
     (
         {
             "method": "POST",
-            "transaction_id": uuid4(),
+            "transaction_id": str(uuid4()),
             "data": {
                 "test": 1,
             },
@@ -18,7 +18,7 @@ import pytest
     (
         {
             "method": "GET",
-            "transaction_id": uuid4(),
+            "transaction_id": str(uuid4()),
             "data": {
                 "test": 1,
             }
@@ -27,7 +27,7 @@ import pytest
     (
         {
             "method": "PUT",
-            "transaction_id": uuid4(),
+            "transaction_id": str(uuid4()),
             "data": {
                 "test": 1,
             }
@@ -36,7 +36,7 @@ import pytest
     (
         {
             "method": "DELETE",
-            "transaction_id": uuid4(),
+            "transaction_id": str(uuid4()),
             "data": {
                 "test": 1,
             }
@@ -46,3 +46,49 @@ import pytest
 def test__validate_script_schema__validate_valid_payload__should_succeed(payload):
     result = validate_script_schema(payload)
     assert result == {}  # Empty dict means no errors
+
+
+@pytest.mark.parametrize('payload, faulty_field', (
+    (
+        {
+            "method": "NOPE",
+            "transaction_id": str(uuid4()),
+            "data": {
+                "test": 1,
+            },
+        },
+        "method",
+    ),
+    (
+        {
+            "method": "POST",
+            "transaction_id": "111",
+            "data": {
+                "test": 1,
+            },
+        },
+        "transaction_id",
+    ),
+    (
+        {
+            "method": "POST",
+            "transaction_id": 111,
+            "data": {
+                "test": 1,
+            },
+        },
+        "transaction_id",
+    ),
+    (
+        {
+            "method": "POST",
+            "transaction_id": str(uuid4()),
+            "data": "1214123",
+        },
+        "data",
+    ),
+))
+def test__validate_script_schema__validate_invalid_error__should_return_errors(payload, faulty_field):
+    result = validate_script_schema(payload)
+    assert faulty_field in result.keys()
+    assert len(result) == 1
