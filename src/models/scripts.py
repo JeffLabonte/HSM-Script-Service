1c5391ef6762ce488e46e83b713538aa46114a67
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-24 23:12:45 -0500

    Update database schema

--- src/models/scripts.py
+++ src/models/scripts.py
@@ -1,14 +1,20 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, relationship
 
 from models.base import BASE
+
+
+class GitRepository(BASE):
+    id = Column(Integer, primary_key=True)
+    repository = Column(String(256))
 
 
 class Scripts(BASE):
     __tablename__ = 'scripts'
 
     id = Column(Integer, primary_key=True)
-    name = Column("script_name", String(64))
-    path = Column("script_path", String(256))
+    name = Column("name", String(64))
+    repository = relationship('GitRepository')
+    exec = Column("exec", String(256))
 
     def __repr__(self):
         return f"<Script(id={self.id}, name={self.name}, path={self.path})>"
