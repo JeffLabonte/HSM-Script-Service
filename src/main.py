d4bb2e9afbffd69681a773e6daf0cb4c7927a39d
Jeff Labonte <grimsleepless@protonmail.com>
2021-02-06 17:31:12 -0500

    Add function to handle environment

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
-# import pika
-
 import logging
+from time import sleep
+from os import environ
 
 from helper.amqp import get_payload_from_body
 
 logger = logging.getLogger('script_service')
 
 TOPIC_TO_CONSUME = 'script.#'
+
+
+def get_environment(variable, default):
+    if value := environ.get(variable, None):
+        return value
+    return default
 
 
 def callback(ch, method, properties, body):
