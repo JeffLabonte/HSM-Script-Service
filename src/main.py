baca664d69afee35b5a241f7fbcd52a2b8a78097
Jeff Labonte <grimsleepless@protonmail.com>
2021-02-27 01:28:18 -0500

    Reformat and change prints for loggers

--- src/main.py
+++ src/main.py
@@ -4,10 +4,12 @@ from os import environ
 
 from helper.amqp import get_payload_from_body
 from service.amqp import AMQPService
+from logger import configure_logger
 
-logger = logging.getLogger('script_service')
+configure_logger()
+logger = logging.getLogger("script_service")
 
-TOPIC_TO_CONSUME = 'script.#'
+TOPIC_TO_CONSUME = "script.#"
 
 AMQP = None
 
@@ -20,20 +22,16 @@ def get_environment(variable, default):
 
 def callback(ch, method, properties, body):
     payload = get_payload_from_body(body)
-    print(f"ch: {ch}")
-    print(f"method: {method}")
-    print(f"properties: {properties}")
-    print(f"body: {body}")
-    print(payload)
-    AMQP.publish(topic='script.create.response', message={"test": "test"})
+    logger.warning(f"ch: {ch}")
+    logger.warning(f"method: {method}")
+    logger.warning(f"properties: {properties}")
+    logger.warning(f"body: {body}")
+    logger.warning(payload)
+    AMQP.publish(topic="script.create.response", message={"test": "test"})
 
 
 if __name__ == "__main__":
     broker_host = get_environment(variable="BROKER_HOST", default="broker")
-    AMQP = AMQPService(
-        callback,
-        host=broker_host,
-        routing_keys=['script.create']
-    )
-    print("Starting service")
+    AMQP = AMQPService(callback, host=broker_host, routing_keys=["script.create"])
+    logger.info("Starting service")
     AMQP.start_consuming()
